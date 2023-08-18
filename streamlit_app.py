621e9dd85cf5132cd531e87b8de5ab45aa76b318
cturhan74 <142581753+cturhan74@users.noreply.github.com>
2023-08-18 11:35:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 TEXT: Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 TEXT: Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 TEXT: Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞TEXT: Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
