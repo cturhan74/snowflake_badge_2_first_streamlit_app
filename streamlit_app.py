aa86975cce0aa97ff87963ad4971f017b30bc605
cturhan74 <142581753+cturhan74@users.noreply.github.com>
2023-08-18 11:31:03 +0200

    Update streamlit_app.py
    add new Textlines and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New History Diner')
+
+streamlit.header('HEADER: Breakfast Menu')
+streamlit.text('TEXT: Omega 3 & Blueberry Oatmeal')
+streamlit.text('TEXT: Kale, Spinach & Rocket Smoothie')
+streamit.text('TEXT: Hard-Boiled Free Range Egg')
