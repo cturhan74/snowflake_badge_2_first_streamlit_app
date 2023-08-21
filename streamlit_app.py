e7d1237db1b33c964708e260bf6ea2278ec77e32
cturhan74 <142581753+cturhan74@users.noreply.github.com>
2023-08-21 11:14:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,13 +43,18 @@ except URLError as e:
   
 import snowflake.connector
 
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')");
+      return "Thanks for adding " + new_fruit
 
-
-
-
-
-
-
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+if stremlit.button ('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  
+streamlit.write('The user entered ', add_my_fruit)
 
 
 streamlit.header("The fruit load list contains: ")
@@ -64,10 +69,5 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = my_cur.fetchall()
     streamlit.dataframe(my_data_rows)
 
-# 🎯 Can You Add A Second Text Entry Box? 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
 
-# This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
